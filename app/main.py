0a16bbb468a53322ba86bb96f8430b78f372e804
Jerry. 백광현 <bbbb0221@tessa.art>
2025-07-05 14:53:09 +0900

    feat: params test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-from typing import Union
+from fastapi import FastAPI, Query, Path
 import logging
-
+from enum import Enum
+from typing import Union,Annotated, Literal
+from pydantic import BaseModel, Field
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,7 +14,78 @@ def read_root():
     logger.info("루트 엔드포인트 호출됨")
     return {"message": "Hello, FastAPI!"}
 
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_items_query(
+    q: Union[str, None] = Query(
+        default=None,
+        alias="item-query",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        max_length=50,
+        pattern="^fixedquery$",
+        deprecated=True,
+    ),
+):
+    results: dict[str, list[dict[str, str]]] = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results["items"].append({ "q": q})
+    return results
+
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+
+@app.get("/items/")
+async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    return filter_query
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    logger.info(f"아이템 조회: item_id={item_id}, q={q}")
-    return {"item_id": item_id, "q": q}
+async def read_item(
+      item_id: int = Path(title="The ID of the item to get", gt=0, le=1000),
+    q: Union[str, None] = Query(default=None, alias="item-query"),
+):
+    results: dict = {"item_id": item_id}
+    if q:
+        results["q"] = q
+    return results
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
